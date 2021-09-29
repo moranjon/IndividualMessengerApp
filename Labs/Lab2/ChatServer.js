0b9e42b70afcaa43b36f305bb7b561e5b67ae860
Jonathan Moran <moranj13@udayton.edu>
2021-09-29 23:02:00 +0000

    updated Lab2

--- Labs/Lab2/ChatServer.js
+++ Labs/Lab2/ChatServer.js
@@ -8,21 +8,38 @@ app.get('/', (request, response) => {
   console.log("Got an HTTP request")  
   response.sendFile(__dirname+'/index.html')
 })
+
 var io = require('socket.io');
 var socketio = io.listen(server);
 console.log("Socket.IO is listening at port: " + port);
 socketio.on("connection", function (socketclient) {
+       
     console.log("A new Socket.IO client is connected. ID= " + socketclient.id);
-    socketclient.on("login", (username) => {
-        socketclient.username = username;
-        var welcomemessage = username + " has joined the chat system!";
-        console.log(welcomemessage);
-        socketio.sockets.emit("welcome", welcomemessage);
+    socketclient.on("login", (username, password) => {
+        console.log("Debug>Got username="+username + ";password="+password);
+        if(DataLayer.checklogin(username,password)){
+            socketclient.authenticated=true;
+            socketclient.emit("authenticated");
+            socketclient.username=username;
+            var welcomemessage = username + " has joined the chat system!";
+            console.log(welcomemessage);
+            //socketio.sockets.emit("welcome", welcomemessage);
+            SendToAuthenticatedClient(socketclient,"welcome",welcomemessage)
+        }
+        //socketclient.username = username;
+        //var welcomemessage = username + " has joined the chat system!";
+        //console.log(welcomemessage);
+        //socketio.sockets.emit("welcome", welcomemessage);
     });
     socketclient.on("chat", (message) => {
+        if(!socketclient.authenticated){
+            console.log("Unauthenticated client sent a chat. Supress!");
+            return;
+        }        
         var chatmessage = socketclient.username + " says: " + message;
         console.log(chatmessage);
-        socketio.sockets.emit("chat", chatmessage);
+        //socketio.sockets.emit("chat", chatmessage);
+        SendToAuthenticatedClient(undefined,"chat",chatmessage);
     });
 });
 var DataLayer = {
@@ -32,5 +49,18 @@ var DataLayer = {
         console.log("checklogin: " + username + "/" + password);
         console.log("Just for testing - return true");
         return true;
+    }
+}
+
+function SendToAuthenticatedClient(sendersocket,type,data){
+    var sockets = socketio.sockets.sockets;
+    for(var socketId in sockets) {
+        var socketclient = sockets[socketId];
+        if(socketclient.authenticated){
+            socketclient.emit(type,data);
+            var logmsg= "Debug:>sent to" +
+                socketclient.username + "with ID=" + socketId;
+            console.log(logmsg);
+        }
     }
 }
